795d8be727baa2e465a5a344518661202a6c971b
nehakrishnakumar <nehakk.2014@gmail.com>
2021-01-14 11:59:15 -0500

    Started table and added completions.json. First steps of proj

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import React from "react";
 import Welcome from "../components/welcome";
 import Layout from "../components/layout";
 import SEO from "../components/seo";
+import Table from "../components/table";
 
 function IndexPage() {
   return (
