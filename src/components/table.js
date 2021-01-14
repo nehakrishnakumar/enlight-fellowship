c3ecd32748afaadc83855edc34f8d0addce49800
nehakrishnakumar <nehakk.2014@gmail.com>
2021-01-14 15:36:03 -0500

    Fixed some errors w/ table creation

--- src/components/table.js
+++ src/components/table.js
@@ -1,20 +1,17 @@
 function Table() {
-	var completions = require('./completions.json');
-	var tr, td;
-	tbody = matchData.querySelector('tbody');
+var completions = require('./completions.json');
+var tr, td;
 	for (var i = 0; i < completions.length; i++) {
-		tr = tbody.insertRow(tbody.rows.length);
+		tr = completions.insertRow(completions.rows.length);
 		td = tr.insertCell(tr.cells.length);
-       		td.innerHTML = completions[i].name;
-       		td = tr.insertCell(tr.cells.length);
-       		td.innerHTML = completions[i].username;
-       		td = tr.insertCell(tr.cells.length);
-       		td.innerHTML = completions[i].milestones;
-       		td = tr.insertCell(tr.cells.length);
-   
-	 }
-
-
-
+		td.innerHTML = completions[i].name;
+		td = tr.insertCell(tr.cells.length);
+		td.innerHTML = completions[i].username;
+		td = tr.insertCell(tr.cells.length);
+		td.innerHTML = completions[i].milestones;
+		td = tr.insertCell(tr.cells.length); 
+	
+	}
 
 }
+export default Table;
