9b3a3eb78f8d069f8ac9d094139ba6ceba2133f6
nehakrishnakumar <nehakk.2014@gmail.com>
2021-01-14 21:30:54 -0500

    New table approach

--- src/components/table.js
+++ src/components/table.js
@@ -1,10 +1,35 @@
+import React from "react";
+import { Link } from "gatsby";
 function Table() {
 	return (
-
-
-
-
-
-
-);
+		var table = document.createElement("table");
+		var tr = table.insertRow(-1);
+		for (var i = 0; i < cols.length; i++) { 
+                  
+                // Create the table header th element 
+                	var theader = document.createElement("th"); 
+                	theader.innerHTML = cols[i]; 
+                  
+                // Append columnName to the table row 
+                	tr.appendChild(theader); 
+           	}
+		// Adding the data to the table 
+            	for (var i = 0; i < list.length; i++) { 
+                  
+                // Create a new row 
+                	trow = table.insertRow(-1); 
+                	for (var j = 0; j < cols.length; j++) { 
+                    		var cell = trow.insertCell(-1); 
+                      
+                    // Inserting the cell at particular place 
+                    		cell.innerHTML = list[i][cols[j]]; 
+               		 } 
+            } 
+              
+            // Add the newely created table containing json data 
+            var el = document.getElementById("table"); 
+            el.innerHTML = ""; 
+            el.appendChild(table); 
+	);
 }
+export default Table;
