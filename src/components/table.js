b176b37820e7fdd9f2000612293387c61c393f29
nehakrishnakumar <nehakk.2014@gmail.com>
2021-01-14 21:17:27 -0500

    Reset table.js for next approach

--- src/components/table.js
+++ src/components/table.js
@@ -1,17 +1,10 @@
 function Table() {
-var completions = require('./completions.json');
-var tr, td;
-	for (var i = 0; i < completions.length; i++) {
-		tr = completions.insertRow(completions.rows.length);
-		td = tr.insertCell(tr.cells.length);
-		td.innerHTML = completions[i].name;
-		td = tr.insertCell(tr.cells.length);
-		td.innerHTML = completions[i].username;
-		td = tr.insertCell(tr.cells.length);
-		td.innerHTML = completions[i].milestones;
-		td = tr.insertCell(tr.cells.length); 
-	
-	}
+	return (
 
+
+
+
+
+
+);
 }
-export default Table;
